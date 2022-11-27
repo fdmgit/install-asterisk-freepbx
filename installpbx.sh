b88d725d54a344c91bba103521a91eb16e8eb101
Flavio Del Mestre <flavio@perunis.ch>
2022-11-27 20:51:33 +0100

    Update installpbx.sh

--- installpbx.sh
+++ installpbx.sh
@@ -288,7 +288,7 @@ apt-get update
 
 apt-get install php7.4 php7.4-mysql php7.4-cgi php7.4-cli php7.4-common php7.4-imap php7.4-ldap \
 php7.4-xml php7.4-fpm php7.4-curl php7.4-mbstring php7.4-zip php7.4-gd php7.4-xml php7.4-json \
-php7.4-bcmath php-pear -y
+php7.4-bcmath php7.4-sqlite php-pear -y
 
 apt-get install libapache2-mod-php7.4 -y
 
